8f4c872e54d95d45c5f54876192f4270a894a44c
rukhasa <mr.khamazaev@mail.ru>
2023-10-21 15:25:55 +0400

    HW_1 updated

--- project-template/src/main/java/edu/hw1/Main.java
+++ project-template/src/main/java/edu/hw1/Main.java
@@ -1,48 +1,49 @@
 package edu.hw1;
 
-
 // Use this editor to write, compile and run your Java code online
+
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-class HelloWorld {
-    public static int minutesToSeconds(String time){
-        int n = time.length();
-        Boolean err = false;
+class Main {
+    public static int minutesToSeconds(String time) {
+        var input = time.split(":");
 
-        if (time.charAt(n-3)!= ':'){
-            err = true;// wrong input
+        boolean err = input.length != 2;
+        if (err) {
+            return -1;
         }
+
         int secs = 0;
         try {
-            secs = Integer.parseInt(time.substring(n-2));
+            secs = Integer.parseInt(input[1]);
 
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             err = true;
         }
-        if (secs>59 || secs<0){
+        if (secs > 59 || secs < 0) {
             err = true;
         }
         int mins = 0;
         try {
-            mins = Integer.parseInt(time.substring(0, n-3));
+            mins = Integer.parseInt(input[0]);
+        } catch (NumberFormatException e) {
+            err = true;// wrong input
         }
-        catch (NumberFormatException e) {
-            err=true;// wrong input
-        }
-        if (err){
+        if (err) {
             return -1;
         }
-        return mins*60 + secs;
+        return mins * 60 + secs;
 
         //return n;
     }
-    public static int countDigits(int n){
+
+    public static int countDigits(int n) {
         String str = String.valueOf(n);
         return str.length();
     }
-    public static Boolean isNestable(int[] a, int[] b){
+
+    public static boolean isNestable(int[] a, int[] b) {
         int n = a.length;
         int m = b.length;
         int minA = a[0];
@@ -50,57 +51,57 @@ class HelloWorld {
         int maxA = a[0];
         int maxB = b[0];
 
-        for (int i=0; i<n;++i){
-            if (a[i]<minA){
+        for (int i = 0; i < n; ++i) {
+            if (a[i] < minA) {
                 minA = a[i];
             }
-            if(a[i]>maxA){
-                maxA=a[i];
+            if (a[i] > maxA) {
+                maxA = a[i];
             }
         }
-        for (int i=0; i<m;++i){
-            if (b[i]<minB){
+        for (int i = 0; i < m; ++i) {
+            if (b[i] < minB) {
                 minB = b[i];
             }
-            if(b[i]>maxB){
+            if (b[i] > maxB) {
                 maxB = b[i];
             }
         }
-        return maxA < maxB && minA>minB;
+        return maxA < maxB && minA > minB;
 
     }
-    public static String fixString(String str){
+
+    public static String fixString(String str) {
         int n = str.length();
 
         char[] newstr = new char[n];
-        if (n%2 == 1){
+        if (n % 2 == 1) {
 
             n--;
-            newstr[n]=str.charAt(n);
+            newstr[n] = str.charAt(n);
         }
-        for (int i = 0; i < n; i+=2){
-            newstr[i] = str.charAt(i+1);
-            newstr[i+1] = str.charAt(i);
+        for (int i = 0; i < n; i += 2) {
+            newstr[i] = str.charAt(i + 1);
+            newstr[i + 1] = str.charAt(i);
         }
         return new String(newstr);
     }
-    public static Boolean isPalindromeDescendant(int num){
+
+    public static Boolean isPalindromeDescendant(int num) {
         String s = String.valueOf(num);
         int n = s.length();
-        if (n<2){
+        if (n < 2) {
             return false;
         }
-        Boolean isPal = true;
+        boolean isPal = true;
         int newnum = 0;
-        int[] power = {1,10,100,1000,10000};
-        for(int i = 0; i < n; i+=2)
-        {
+        int[] power = {1, 10, 100, 1000, 10000};
+        for (int i = 0; i < n; i += 2) {
             //System.out.println(i/2 - 1);
-            newnum+= (s.charAt(i) - '0' + s.charAt(i+1) - '0') * power[(n-i)/2 - 1];
+            newnum += (s.charAt(i) - '0' + s.charAt(i + 1) - '0') * power[(n - i) / 2 - 1];
         }
-        for(int i = 0; i < n/2 ; i++)
-        {
-            if(s.charAt(i) != s.charAt(n-i-1)){
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
                 isPal = false;
                 break;
             }
@@ -108,59 +109,64 @@ class HelloWorld {
         //System.out.println(newnum);
         return isPal || isPalindromeDescendant(newnum);
     }
-    public static void helloWorld(){
+
+    public static void helloWorld() {
         java.util.logging.Logger logger
             = Logger.getLogger(
-            HelloWorld.class.getName());
+            Main.class.getName());
 
         // Call info method
         logger.info("Привет, Мир!");
     }
 
-    public static int countK(int n){
+    public static int countK(int n) {
         int kapnum = 6174;
-        if (n == kapnum){
+        if (n == kapnum) {
             return 0;
         }
+
         var a = new int[4];
-        int[] power = {1,10,100,1000};
-        for(int i = 0; i < 4; ++i){
-            a[i] = n/power[i]%10;
+        int[] power = {1, 10, 100, 1000};
+        for (int i = 0; i < 4; ++i) {
+            a[i] = n / power[i] % 10;
+        }
+        if (a[0] == a[1] && a[1] == a[2] && a[2] == a[3]) {
+            return -1;
         }
         Arrays.sort(a);
 
         int newnum = 0;
-        for(int i= 0; i < 4; ++i){
-            newnum+= (a[i]-a[3-i] )*power[i];
+        for (int i = 0; i < 4; ++i) {
+            newnum += (a[i] - a[3 - i]) * power[i];
         }
         //System.out.println(newnum);
         //return 0;
         return countK(newnum) + 1;
 
-
     }
-    public static int rotateLeft(int n, int shift){
-        int len=0;
+
+    public static int rotateLeft(int n, int shift) {
+        int len = 0;
         int x = n;
-        while(x>0){
-            x/=2;
+        while (x > 0) {
+            x /= 2;
             len++;
         }
-        var power = new int[len+1];
+        var power = new int[len + 1];
         power[0] = 1;
-        for(int i= 1; i < len+1; ++i){
-            power[i] = power[i-1]*2;
+        for (int i = 1; i < len + 1; ++i) {
+            power[i] = power[i - 1] * 2;
         }
-        while(shift>0){
-            Boolean flag= false;
-            n*=2;
-            if(n>power[len]-1){
+        while (shift > 0) {
+            boolean flag = false;
+            n *= 2;
+            if (n > power[len] - 1) {
                 flag = true;
             }
 
-            if(flag){
-                n-=power[len];
-                n+=1;
+            if (flag) {
+                n -= power[len];
+                n += 1;
                 //System.out.println(n);
             }
 
@@ -169,29 +175,29 @@ class HelloWorld {
         }
         return n;
     }
-    public static int rotateRight(int n, int shift){
-        int len=0;
+
+    public static int rotateRight(int n, int shift) {
+        int len = 0;
         int x = n;
-        while(x>0){
-            x/=2;
+        while (x > 0) {
+            x /= 2;
             len++;
         }
-        var power = new int[len+1];
+        var power = new int[len + 1];
         power[0] = 1;
-        for(int i= 1; i < len+1; ++i){
-            power[i] = power[i-1]*2;
+        for (int i = 1; i < len + 1; ++i) {
+            power[i] = power[i - 1] * 2;
         }
-        while(shift>0){
-            Boolean flag= false;
-            if(n%2==1){
+        while (shift > 0) {
+            boolean flag = false;
+            if (n % 2 == 1) {
                 flag = true;
             }
-            n/=2;
-            if(flag){
-                n+=power[len-1];
+            n /= 2;
+            if (flag) {
+                n += power[len - 1];
                 //System.out.println(n);
-            }
-            else{
+            } else {
                 len--;
             }
             shift--;
@@ -199,31 +205,31 @@ class HelloWorld {
         }
         return n;
     }
-    public static Boolean knightBoardCapture(int[][] field){
+
+    public static Boolean knightBoardCapture(int[][] field) {
         int n = 8;
+        int[][] zuge = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; j++) {
+                if (field[i][j] == 1) {
+                    for (int[] zug : zuge) {
+                        int row = i + zug[0];
+                        int col = j + zug[1];
 
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n; j++){
-                if(field[i][j]==1){
-                    for(int x = 0; x < n; ++x){
-                        for(int y = 0; y < n; y++){
-                            if(field[x][y]==1){
-                                if(Math.abs(x-i) ==2 && Math.abs(y-j)==1 ||Math.abs(x-i) ==1 && Math.abs(y-j)==2  ){
-                                    return false;
-                                }
-                            }
+                        if ((row >= 0 && row < n && col >= 0 && col < n) && field[row][col] == 1) {
+                            return false;
                         }
-
                     }
                 }
             }
         }
         return true;
     }
+
     public static void main(String[] args) {
         System.out.println(minutesToSeconds("324:12"));
-        int[] a = { 1,2,3,4};
-        int[] b = {2,3};
+        int[] a = {1, 2, 3, 4};
+        int[] b = {2, 3};
         int[][] chess = {
             {0, 0, 0, 1, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0},
@@ -253,7 +259,7 @@ class HelloWorld {
             {0, 0, 0, 0, 0, 1, 0, 0},
             {1, 0, 0, 0, 0, 0, 0, 0}
         };
-        System.out.println(isNestable(a,b));
+        System.out.println(isNestable(a, b));
         System.out.println(fixString("badce"));
         System.out.println(fixString("123456"));
         System.out.println(fixString("hTsii  s aimex dpus rtni.g"));
@@ -270,10 +276,6 @@ class HelloWorld {
         System.out.println(isPalindromeDescendant(13001120));
         System.out.println(isPalindromeDescendant(23336014));
         helloWorld();
-
-
-
-
-
+        System.out.println(countK(2222));
     }
 }
--- project-template/src/test/java/edu/hw1/SampleTest.java
+++ project-template/src/test/java/edu/hw1/SampleTest.java
@@ -1,72 +1,105 @@
 package edu.hw1;
 
+import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.*;
-class hw1Test {
+
+class SampleTest {
 
     @org.junit.jupiter.api.Test
     void Task1() {
-        assert (HelloWorld.minutesToSeconds("01:00") == 60);
-        assert (HelloWorld.minutesToSeconds("13:56") == 836);
-        assert (HelloWorld.minutesToSeconds("10:60") == -1);
+        int expected = 60;
+        int actual = Main.minutesToSeconds("01:00");
+        Assertions.assertEquals(expected, actual);
+        int expected2 = 836;
+        int actual2 = Main.minutesToSeconds("13:56");
+        Assertions.assertEquals(expected2, actual2);
+        int expected3 = -1;
+        int actual3 = Main.minutesToSeconds("10:60");
+        Assertions.assertEquals(expected3, actual3);
+
     }
 
     @org.junit.jupiter.api.Test
     void Task2() {
-        assert (HelloWorld.countDigits(4666)== 4);
-        assert (HelloWorld.countDigits(544)== 3);
-        assert (HelloWorld.countDigits(0)== 1);
-
+        int expected = 4;
+        int actual = Main.countDigits(4666);
+        Assertions.assertEquals(expected, actual);
+        int expected2 = 3;
+        int actual2 = Main.countDigits(544);
+        Assertions.assertEquals(expected2, actual2);
+        int expected3 = 1;
+        int actual3 = Main.countDigits(0);
+        Assertions.assertEquals(expected3, actual3);
 
     }
 
     @org.junit.jupiter.api.Test
     void Task3() {
-        int[] a = { 1,2,3,4};
-        int[] b = {0,6};
-        int[] a2 = { 3, 1};
+        int[] a = {1, 2, 3, 4};
+        int[] b = {0, 6};
+        int[] a2 = {3, 1};
         int[] b2 = {4, 0};
-        int[] a3= { 9, 9, 8};
+        int[] a3 = {9, 9, 8};
         int[] b3 = {8, 9};
-        int[] a4= { 1, 2, 3, 4};
+        int[] a4 = {1, 2, 3, 4};
         int[] b4 = {2, 3};
         /*isNestable([3, 1], [4, 0]) -> true
 isNestable([9, 9, 8], [8, 9]) -> false
 isNestable([1, 2, 3, 4], [2, 3]) -> false */
-        assert HelloWorld.isNestable(a,b);
-        assert HelloWorld.isNestable(a2,b2);
-        assert !HelloWorld.isNestable(a3,b3) ;
-        assert !HelloWorld.isNestable(a4,b4) ;
+
+        Assertions.assertTrue(Main.isNestable(a, b));
+        Assertions.assertTrue(Main.isNestable(a2, b2));
+        Assertions.assertFalse(Main.isNestable(a3, b3));
+        Assertions.assertFalse(Main.isNestable(a4, b4));
+
     }
 
     @org.junit.jupiter.api.Test
     void Task4() {
-        assert HelloWorld.fixString("badce").equals("abcde");
-        assert HelloWorld.fixString("123456").equals("214365");
-        assert HelloWorld.fixString("hTsii  s aimex dpus rtni.g").equals("This is a mixed up string.");
+        var expected = "abcde";
+        var actual = Main.fixString("badce");
+        Assertions.assertEquals(expected, actual);
+        var expected2 = "214365";
+        var actual2 = Main.fixString("123456");
+        Assertions.assertEquals(expected2, actual2);
+        var expected3 = "This is a mixed up string.";
+        var actual3 = Main.fixString("hTsii  s aimex dpus rtni.g");
+        Assertions.assertEquals(expected3, actual3);
     }
 
     @org.junit.jupiter.api.Test
     void Task5() {
-        assert HelloWorld.isPalindromeDescendant(11211230);
-        assert HelloWorld.isPalindromeDescendant(13001120);
-        assert HelloWorld.isPalindromeDescendant(23336014);
+        Assertions.assertTrue(Main.isPalindromeDescendant(11211230));
+        Assertions.assertTrue(Main.isPalindromeDescendant(13001120));
+        Assertions.assertTrue(Main.isPalindromeDescendant(23336014));
     }
 
     @org.junit.jupiter.api.Test
     void Task6() {
-        assert HelloWorld.countK(6621) == 5;
-        assert HelloWorld.countK(6554) == 4;
-        assert HelloWorld.countK(1234) == 3;
+        int expected = 5;
+        int actual = Main.countK(6621);
+        Assertions.assertEquals(expected, actual);
+        int expected2 = 4;
+        int actual2 = Main.countK(6554);
+        Assertions.assertEquals(expected2, actual2);
+        int expected3 = 3;
+        int actual3 = Main.countK(1234);
+        Assertions.assertEquals(expected3, actual3);
+
     }
 
     @org.junit.jupiter.api.Test
     void Task7() {
-        assert HelloWorld.rotateLeft(16,1) == 1;
-        assert HelloWorld.rotateLeft(17,2) == 6;
-        assert HelloWorld.rotateRight(8,1) == 4;
+        int exptected = 1;
+        int exptected2 = 6;
+        int exptected3 = 4;
+        int actual = Main.rotateLeft(16, 1);
+        int actual2 = Main.rotateLeft(17, 2);
+        int actual3 = Main.rotateRight(8, 1);
+        Assertions.assertEquals(exptected, actual);
+        Assertions.assertEquals(exptected2, actual2);
+        Assertions.assertEquals(exptected3, actual3);
     }
-
-
 
     @org.junit.jupiter.api.Test
     void Task8() {
@@ -99,9 +132,9 @@ isNestable([1, 2, 3, 4], [2, 3]) -> false */
             {0, 0, 0, 0, 0, 1, 0, 0},
             {1, 0, 0, 0, 0, 0, 0, 0}
         };
-        assert HelloWorld.knightBoardCapture(chess);
-        assert !HelloWorld.knightBoardCapture(chess2);
-        assert !HelloWorld.knightBoardCapture(chess3);
+        Assertions.assertTrue(Main.knightBoardCapture(chess));
+        Assertions.assertFalse(Main.knightBoardCapture(chess2));
+        Assertions.assertFalse(Main.knightBoardCapture(chess3));
     }
 
     @org.junit.jupiter.api.Test
